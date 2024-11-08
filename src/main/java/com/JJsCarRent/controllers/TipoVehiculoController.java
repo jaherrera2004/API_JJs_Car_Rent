61e6ea1eee486c99486883d053fc28e1d0e78fbb
herrECoDe <juanandresh1024@gmail.com>
2024-11-08 15:18:37 -0500

    prueba1

--- src/main/java/com/JJsCarRent/controllers/TipoVehiculoController.java
+++ src/main/java/com/JJsCarRent/controllers/TipoVehiculoController.java
@@ -6,6 +6,7 @@ import com.JJsCarRent.services.TipoVehiculo.TipoVehiculoIService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,8 +22,10 @@ public class TipoVehiculoController {
     @Operation(summary = "Agregar tipo de vehiculo")
     @PreAuthorize("hasAuthority('tipo-vehiculo:agregar')")
     @PostMapping
-    public void agregarTipoVehiculo(@RequestBody @Valid TipoVehiculoRequest request){
+    public ResponseEntity<Void> agregarTipoVehiculo(@RequestBody @Valid TipoVehiculoRequest request){
         tipoVehiculoIService.agregarTipoVehiculo(request);
+        return ResponseEntity.ok()
+                .build();
     }
 
     @Operation(summary = "Obtener lista de tipos de vehiculo")
