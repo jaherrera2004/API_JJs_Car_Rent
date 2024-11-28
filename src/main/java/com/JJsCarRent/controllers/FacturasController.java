2c32e55bc937db672427bae067ab2a56e1785644
herrECoDe <juanandresh1024@gmail.com>
2024-11-27 19:43:44 -0500

    añadiendo autorizacion a las facturas

--- src/main/java/com/JJsCarRent/controllers/FacturasController.java
+++ src/main/java/com/JJsCarRent/controllers/FacturasController.java
@@ -3,6 +3,7 @@ package com.JJsCarRent.controllers;
 import com.JJsCarRent.models.dto.FacturaDto;
 import com.JJsCarRent.services.facturas.FacturaIService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class FacturasController {
     private final FacturaIService facturaIService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('facturas:obtener-lista') ")
     public List<FacturaDto> obtenerFacturas() {
         return facturaIService.obtenerFacturas();
     }
